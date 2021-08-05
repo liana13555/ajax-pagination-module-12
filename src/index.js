c04c9c1171b07cb3bd9dd4a36e7b051d01663d0b
Liana Baryshova <barishovaliana@gmail.com>
2021-08-05 21:49:18 +0200

    Update files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './css/styles.css';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
 
+
 const pagination = new Pagination('#tui-pagination-container', {
   totalItems: 0,
   itemsPerPage: 20,
@@ -11,19 +12,20 @@ const pagination = new Pagination('#tui-pagination-container', {
 });
 
 const page = pagination.getCurrentPage();
+
 fetchImages(page).then(data => {
   pagination.reset(data.total);
   renderImages(data.images);
 });
 
-pagination.on('afterMove', event => {
+pagination.on('afterMove', (event) => {
   const currentPage = event.page;
   fetchImages(currentPage).then(data => renderImages(data.images));
 });
 
 function fetchImages(page) {
   return fetch(
-    `https://pixabay.com/api/?key=4823621-792051e21e56534e6ae2e472f&q=sun&page=${page}&per_page=20`
+    `https://pixabay.com/api/?key=22568340-3d930d703d1ad37110880a9ab&q=sun&page=${page}&per_page=20`
   )
     .then(res => res.json())
     .then(data => ({ images: data.hits, total: data.totalHits }));
